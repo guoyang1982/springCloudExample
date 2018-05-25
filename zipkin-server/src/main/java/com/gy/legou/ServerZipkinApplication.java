4b63f2f29390f19d3714e909ef38ada255460002
guoyang9 <guoyang9@le.com>
2018-05-25 11:51:45 +0800

    3333

--- zipkin-server/src/main/java/com/gy/legou/ServerZipkinApplication.java
+++ zipkin-server/src/main/java/com/gy/legou/ServerZipkinApplication.java
@@ -2,7 +2,7 @@ package com.gy.legou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+//import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 /**
@@ -11,8 +11,8 @@ import zipkin.server.EnableZipkinServer;
  * @date 2018/5/23 上午10:34
  */
 @SpringBootApplication
-//@EnableZipkinServer //接口方式http
-@EnableZipkinStreamServer //使用Stream方式启动ZipkinServer kafka rabitmq
+@EnableZipkinServer //接口方式http
+//@EnableZipkinStreamServer //使用Stream方式启动ZipkinServer kafka rabitmq
 public class ServerZipkinApplication {
 
     public static void main(String[] args) {
